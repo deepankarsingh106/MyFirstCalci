e8f4d7150c3fa1b2befa2e31a94fe87320fee7f7
deepankarsingh106 <deepankarsingh106@gmail.com>
2026-07-05 22:34:09 +0530

    Calculator

---
+++ script.js
@@ -0,0 +1,37 @@
+// basic functioning of the Calculator
+const display = document.getElementById("display");
+function append(value){
+    display.value += value;
+}
+function clearDisplay(){
+    display.value = "";
+}
+function deleteLast(){
+    display.value = display.value.slice(0,-1);
+}
+function calculate(){
+    try{
+        display.value = eval(display.value);
+    }
+    catch{
+        display.value = "Error";
+    }
+}
+
+// Connecting the Keyboard to my Calculator..
+document.addEventListener("keydown", function (event) {
+    const key = event.key;
+    if ((key >= "0" && key <= "9") || "+-*/.%".includes(key)) {
+        append(key);
+    } 
+    else if (key === "Enter") {
+        event.preventDefault();
+        calculate();
+    } 
+    else if (key === "Backspace") {
+        deleteLast();
+    } 
+    else if (key === "Escape") {
+        clearDisplay();
+    }
+});
